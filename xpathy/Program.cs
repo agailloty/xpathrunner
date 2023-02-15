f6709974c50725da8fe4324aeaf591501a4999de
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2023-02-15 21:34:01 +0100

    Ajoutez des fichiers projet.

---
+++ xpathy/Program.cs
@@ -0,0 +1,8 @@
+﻿using HtmlAgilityPack;
+
+var doc = new HtmlDocument();
+doc.Load("jobOffers.html");
+
+var jobTitles = doc.DocumentNode.SelectNodes("//span[starts-with(@id, 'jobTitle')]/text()");
+foreach (var jobTitle in jobTitles)
+    Console.WriteLine(jobTitle.InnerText); 
