50fc6519205845fffc96cc414ef21c16c9cd6f4d
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2023-02-18 11:32:20 +0100

    Update build artifacts to trim only non required assemblies

---
+++ xpathrunner/Program.cs
@@ -0,0 +1,17 @@
+﻿using Cocona;
+using HtmlAgilityPack;
+
+var app = CoconaLiteApp.Create();
+app.AddCommand((string filepath, string xpath) => ApplyOnHTMLFile(filepath, xpath));
+app.Run();
+
+// "//span[starts-with(@id, 'jobTitle')]/text()"
+void ApplyOnHTMLFile(string filepath, string xpath)
+{
+    var doc = new HtmlDocument();
+    doc.Load(filepath);
+
+    var jobTitles = doc.DocumentNode.SelectNodes(xpath);
+    foreach (var jobTitle in jobTitles)
+        Console.WriteLine(jobTitle.InnerText);
+}
