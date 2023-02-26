a258510275225d87d1bfc2bdad7bf17d97c420e1
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2023-02-26 21:02:30 +0100

    Adding Avalonia UI for UI

---
+++ xpathrunnerui/xpathrunnerui.Web/Program.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+using Avalonia.Web;
+using System.Runtime.Versioning;
+using xpathrunnerui;
+
+[assembly: SupportedOSPlatform("browser")]
+
+internal partial class Program
+{
+    private static void Main(string[] args) => BuildAvaloniaApp()
+        .UseReactiveUI()
+        .SetupBrowserApp("out");
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>();
+}
---
+++ xpathrunnerui/xpathrunnerui/ViewModels/MainViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace xpathrunnerui.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
