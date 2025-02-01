733095eadd0f9505109b208fcbd575c167b9ec82
agailloty <mr.clerisaxel@hotmail.fr>
2025-02-01 09:58:41 +0100

    adjust automatically startup heigh and width

--- xpathrunnerui/xpathrunnerui/MainWindow.axaml.cs
+++ xpathrunnerui/xpathrunnerui/MainWindow.axaml.cs
@@ -16,6 +16,13 @@ namespace xpathrunnerui
         public MainWindow()
         {
             InitializeComponent();
+            var screen = Screens.Primary;
+            // Set width and height as percentages of the screen
+            if (screen != null)
+            {
+                Width = screen.WorkingArea.Width * 0.5;
+                Height = screen.WorkingArea.Height * 0.6;
+            }
         }
 
         public async void PickFileHandler(object sender, RoutedEventArgs args)
