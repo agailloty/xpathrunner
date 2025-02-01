f853191c7ab249e1dd0b3ef0c49236270f5ed7bd
agailloty <mr.clerisaxel@hotmail.fr>
2025-02-01 10:08:59 +0100

    Keep only Desktop UI project

--- xpathrunnerui/xpathrunnerui/Program.cs
+++ xpathrunnerui/xpathrunnerui/Program.cs
@@ -2,7 +2,7 @@
 
 using Avalonia;
 
-namespace xpathrunnerui.Desktop
+namespace xpathrunnerui
 {
     internal sealed class Program
     {
@@ -10,14 +10,19 @@ namespace xpathrunnerui.Desktop
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
+        {
+            return AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .WithInterFont()
                 .LogToTrace();
+        }
     }
 }
